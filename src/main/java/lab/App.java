b89e070839ba19a4242a48550fea10f96caeaea4
MARCO GIORGI <marco.giorgi7@studio.unibo.it>
2023-05-11 21:56:58 +0200

    [FIX] clear code

--- src/main/java/lab/App.java
+++ src/main/java/lab/App.java
@@ -1,29 +1,14 @@
 package lab;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-
-import lab.db.ConnectionProvider;
 import lab.model.UserInterface;
 
 public class App {
 
     public static void main(String[] args) throws SQLException {
         
-        ConnectionProvider prov = new ConnectionProvider("root", "Lakanoch98!", "polisportiva");
-        Connection conn= prov.getMySQLConnection();
-        Statement s;
-        ResultSet r;
-        try {
-             s = conn.createStatement();
-            // s.executeUpdate("UPDATE iscritto SET CodSquadra = NULL");
-             
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+       
+        
         UserInterface user =new UserInterface();
         
 
